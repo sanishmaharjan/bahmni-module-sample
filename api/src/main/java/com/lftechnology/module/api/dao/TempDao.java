9f75aaf858ce7273affeb6501fdd198d9f315817
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2017-09-06 10:26:11 +0545

    save & update dao added

--- api/src/main/java/com/lftechnology/module/api/dao/TempDao.java
+++ api/src/main/java/com/lftechnology/module/api/dao/TempDao.java
@@ -1,10 +1,17 @@
 package com.lftechnology.module.api.dao;
 
+import com.lftechnology.module.api.exception.SaveEntityException;
+import com.lftechnology.module.api.exception.UpdateEntityException;
 import com.lftechnology.module.api.model.Temp;
 
 import java.util.List;
 
 public interface TempDao {
+
+    Temp save(Temp temp) throws SaveEntityException;
+
+    Temp update(Temp temp) throws UpdateEntityException;
+
     Temp getTempByUuid(String uuid);
 
     List<Temp> getAllTemps();
--- api/src/main/java/com/lftechnology/module/api/dao/impl/TempDaoImpl.java
+++ api/src/main/java/com/lftechnology/module/api/dao/impl/TempDaoImpl.java
@@ -1,12 +1,16 @@
 package com.lftechnology.module.api.dao.impl;
 
+import com.lftechnology.module.api.exception.SaveEntityException;
+import com.lftechnology.module.api.exception.UpdateEntityException;
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import com.lftechnology.module.api.dao.TempDao;
 import com.lftechnology.module.api.model.Temp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +18,28 @@ import java.util.List;
 public class TempDaoImpl implements TempDao {
     @Autowired
     private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public Temp save(Temp temp) throws SaveEntityException {
+        try {
+            Integer id = (Integer) sessionFactory.getCurrentSession().save(temp);
+            return (Temp) sessionFactory.getCurrentSession().get(Temp.class, id);
+        } catch (HibernateException e) {
+            throw new SaveEntityException("Fail to save doctorAssignment");
+        }
+    }
+
+    @Override
+    @Transactional
+    public Temp update(Temp temp) throws UpdateEntityException {
+        try {
+            sessionFactory.getCurrentSession().saveOrUpdate(temp);
+            return temp;
+        } catch (HibernateException e) {
+            throw new UpdateEntityException("Fail to Update doctorAssignment");
+        }
+    }
 
     @Override
     public Temp getTempByUuid(String uuid) {
